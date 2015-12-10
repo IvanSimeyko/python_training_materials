c5ec69e7f67867f4efe0ecd2c4de8d0c77e3bd69
Ivan Simeyko <iv.simeyko@gmail.com>
2015-12-10 23:59:08 +0200

    solve second task

--- tasks/tasks_1.py
+++ tasks/tasks_1.py
@@ -2,9 +2,12 @@
 
 
 def decode(element):
-    """Decodes a string (AON).
+
+    """
+    Decodes a string (AON).
     Example: 11122234###55' => 1225
     """
+
     #remove duplicates
     temp = ''
     res = ''
@@ -13,7 +16,7 @@ def decode(element):
         if char == i and (len(temp) == 0 or temp[-1] != i):
             temp += i
         char = i
-    #print(temp)
+    #print temp
 
     #replace #
     for sym in temp:
@@ -24,4 +27,27 @@ def decode(element):
         char = sym
     return res
 
-print(decode('11122234###55'))
+print decode('11122234###55')
+
+
+def gnome_sort(element):
+
+    """
+    Gnome sort
+    Example [1, 2, 9, 0, 3, 4] => [0, 1, 2, 3, 4, 9]
+    """
+
+    i = 1
+    while i < len(element):
+        if element[i - 1] <= element[i]:
+            i += 1
+        else:
+            tmp = element[i]
+            element[i] = element[i - 1]
+            element[i - 1] = tmp
+            i -= 1
+            if i == 0:
+                i = 1
+    return element
+
+print gnome_sort([1, 2, 9, 0, 3, 4])
