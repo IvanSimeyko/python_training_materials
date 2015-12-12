9b1e7366dceee5c06012d1f66a24fbd26d789b00
Ivan Simeyko <iv.simeyko@gmail.com>
2015-12-12 15:37:12 +0200

    added func add in interpretator_fouth

--- tasks/interpretator_fouth.py
+++ tasks/interpretator_fouth.py
@@ -1,1 +1,49 @@
+# -*- coding: utf-8 -*-
+res = []
+
+
+def put(element):    # 'put'
+    if element.isdigit():
+        res.append(int(element))
+    else:
+        res.append(element)
+    return res
+
+
+def pop():
+    res.pop()
+    return res
+
+
+def add():
+    if type(res[-1]) == str:
+        summ = ''
+    else:
+        summ = 0
+    for i in range(-2, 0):
+        summ += res[i]
+        res.pop(i)
+    res.append(summ)
+    return res
+
+def sub():
+
+
+lines = (line.rstrip() for line in open('example.rtf'))
+
+for line in lines:
+    if line[:3] == 'put':
+        put(line[4:])
+
+    elif line == 'pop':
+        pop()
+
+    elif line == 'add':
+        if type(res[-1]) == type(res[-2]):
+            add()
+        else:
+            print 'Недопустимые типы данных'
+
+
+print res
 
