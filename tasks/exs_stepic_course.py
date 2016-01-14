d506193b58842af63a2e7159114305ec78c3c58c
Ivan Simeyko <iv.simeyko@gmail.com>
2016-01-14 23:13:55 +0200

    solved task about football team

--- tasks/exs_stepic_course.py
+++ tasks/exs_stepic_course.py
@@ -12,3 +12,57 @@ def circle_len():
     radius = int(input('введите радиус: '))
     length = math.pi * radius * 2
     return length
+
+
+def football_result():
+    """
+    Функция, которая принимает на стандартный вход список игр футбольных команд с результатом матча и выводит на
+    стандартный вывод сводную таблицу результатов всех матчей. Формат ввода следующий:
+    В первой строке указано целое число n — количество завершенных игр.
+    После этого идет n строк, в которых записаны результаты игры в следующем формате:
+    Первая_команда;Забито_первой_командой;Вторая_команда;Забито_второй_командой
+    Вывод программы необходимо оформить следующим образом:
+    Команда:Всего_игр Побед Ничьих Поражений Всего_очков
+    """
+    n = 0
+    result_all_games = []
+    ans = {}
+    result_function = ''
+    games = int(input('Введите количество игр:'))
+    while n < games:
+        res_game = input('Введите результат игры: ').split(';')
+        result_all_games.append(res_game)
+        n += 1
+    #print(result_all_games)
+    for i in result_all_games:
+        # добавляем название команды если ее нет
+        if i[0] not in ans:
+            ans[i[0]] = [0, 0, 0, 0, 0]
+        if i[2] not in ans:
+            ans[i[2]] = [0, 0, 0, 0, 0]
+        # добавляем по одной игре в столбец сыгранных игр
+        ans[i[0]][0] += 1
+        ans[i[2]][0] += 1
+        # заполняем оставшуюся часть таблицы
+        if int(i[1]) < int(i[3]):
+            ans[i[0]][3] += 1
+            ans[i[2]][1] += 1
+            ans[i[2]][4] += 3
+        elif int(i[1]) == int(i[3]):
+            ans[i[0]][2] += 1
+            ans[i[0]][4] += 1
+            ans[i[2]][2] += 1
+            ans[i[2]][4] += 1
+        elif int(i[1]) > int(i[3]):
+            ans[i[2]][3] += 1
+            ans[i[0]][1] += 1
+            ans[i[0]][4] += 3
+    # формируем требуемый вывод для функции
+    for key, value in ans.items():
+        result_function += key + ':'
+        for elem in value:
+            result_function += str(elem) + ' '
+        result_function += '\n'
+    return result_function
+
+print(football_result())
