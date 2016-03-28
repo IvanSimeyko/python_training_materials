a3f91c44872953edbee24ad14d093d31ed5e7ad5
Ivan Simeyko <iv.simeyko@gmail.com>
2016-03-28 21:31:56 +0300

    added reading datafiles

--- tasks/dow_djonce_algoritm.py
+++ tasks/dow_djonce_algoritm.py
@@ -1,13 +1,28 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
-#import seaborn as sns
-#%matplotlib inline
+
+#create variables
+x, y = [0, 0], [0, 0]
+
+#reading data from user
+name_one = raw_input('Введите название первой организации (например AA): ')
+name_two = raw_input('Введите название второй организации (например AXP): ')
+
+#reading file with data
+lines = (line.strip().split(',') for line in open('dow_jones_index.data'))
+for line in lines:
+    if line[1] == name_one:
+        x[0] += float(line[3][1:])
+        x[1] += float(line[6][1:])
+    if line[1] == name_two:
+        y[0] += float(line[3][1:])
+        y[1] += float(line[6][1:])
 
 # creating two massive
-x = np.array([15.82, 16.72, 15.78, 16.42])
-y = np.array([16.71, 16.71, 15.64, 15.97])
-print x, y
+x = np.array(x)
+y = np.array(y)
 
 # plotting massive
 # plt.plot(x, 'r', label='x')
@@ -17,17 +32,15 @@ print x, y
 
 #making a 2d matrix to compute distances between all pairs of x and y
 distances = np.zeros((len(y), len(x)))
-print distances
 
 #use euclidean distance between the pairs of points.
 for i in range(len(y)):
     for j in range(len(x)):
         distances[i, j] = (x[j]-y[i])**2
-
 print distances
 
 
-#write a small function to visualize the distance matrix we just created.
+#function to visualize the distance matrix we just created.
 def distance_cost_plot(distances):
     im = plt.imshow(distances, interpolation='nearest', cmap='Reds')
     plt.gca().invert_yaxis()
